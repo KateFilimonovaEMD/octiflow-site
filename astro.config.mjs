a4de460bb1aa5a3dd7c9536bc9bf672dc0086639
Kate Filimonova <88598106+KateFilimonovaEMD@users.noreply.github.com>
2026-07-28 17:14:25 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://octiflow.app",
-  trailingSlash: "never",
+  trailingSlash: "always",
   build: {
     format: "directory",
   },
