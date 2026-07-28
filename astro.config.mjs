82ba103196b6b92157d55a81250a60c8f723804c
Kate Filimonova <88598106+KateFilimonovaEMD@users.noreply.github.com>
2026-07-28 17:05:24 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://evamariadreams.com",
+  site: "https://octiflow.app",
+  trailingSlash: "never",
   build: {
-    format: "file",
+    format: "directory",
   },
   integrations: [
     mdx(),
-    sitemap({
-      serialize(item) {
-        const url = new URL(item.url);
-
-        if (url.pathname !== "/" && !url.pathname.endsWith(".html")) {
-          url.pathname = `${url.pathname}.html`;
-        }
-
-        return { ...item, url: url.href };
-      },
-    }),
+    sitemap(),
   ],
 });
